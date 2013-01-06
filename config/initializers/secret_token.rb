a8dff702b2448d6e65c7867974d011429d75eb0c
Bob Fung <bob@nuvana.org>
2013-01-06 13:14:03 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fapp::Application.config.secret_token = '9a8dc6d52da5798af1496a6d9299bb6c69e431eb63ca13167b7b481b9f5379992d32e12b573e0d143df7164c96b2d1569af44cc48b2e265ee29434d06960206b'
